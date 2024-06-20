1b2355315978e918389e626f53f1cd9659474dfc
João Lucas Ribeiro de Moraes <joaolucasribeiro03@outlook.com>
2024-06-20 14:14:36 -0300

    deleção de usuario com delete

--- src/main/java/com/joaolucas/workshopmongo/services/UserService.java
+++ src/main/java/com/joaolucas/workshopmongo/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.joaolucas.workshopmongo.domain.User;
 import com.joaolucas.workshopmongo.dto.UserDTO;
@@ -31,6 +32,11 @@ public class UserService {
 
 	public User insert(User obj) {
 		return repo.insert(obj);
+	}
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
 	}
 
 	public User fromDTO(UserDTO objDto) {
